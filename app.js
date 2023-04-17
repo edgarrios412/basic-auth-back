d9d7f6593645176a4e5cd0120ff6b5d9ced9b077
edgarrios412 <edgarrios412@gmail.com>
2023-04-17 17:26:15 -0500

    Push

--- app.js
+++ app.js
@@ -1,20 +1,72 @@
 const express = require("express")
 const cors = require("cors")
 const jwt = require("jsonwebtoken")
+const {User} = require("./db.js")
 
 const app = express ()
 
 app.use(express.json())
 app.use(cors({origin:"*"}))
 
+app.get("/user", (req,res) =>{
+    res.json(User)
+})
+
+app.post("/validate", (req,res) =>{
+    let {token} = req.body
+    token = token.replaceAll("@",".")
+    jwt.verify(token,"secret", (err,user) => {
+        if(err){ return res.status(400).json({error:"La cuenta no pudo ser verificada"})}
+        const userFind = User.find(u => u.user == user.user)
+        userFind.token = null;
+        userFind.verified = true;
+        res.json(userFind)
+    })
+})
+
+app.post("/ban", (req,res) =>{
+    const {user, reason, time} = req.body
+    const userFind = User.find(u => u.user == user)
+    if(userFind){
+        userFind.banned = true;
+        userFind.reason = reason;
+        userFind.time = time;
+        res.json({status:"Banned"})
+    }else{
+    res.json({error:"User not found"})
+    }
+})
+
+
+
 app.post("/login", (req,res) => {
     const {user,pass} = req.body
-    if(user == "admin" && pass == "admin"){
-    const token = jwt.sign(req.body, "secret")
-    res.status(200).json({token})
+    const userFind = User.find(u => u.user == user && u.pass == pass && u.verified == true)
+    if(userFind){
+        if(userFind.banned) res.json({error:"User banned", reason: userFind.reason})
+        const token = jwt.sign(req.body, "secret", {expiresIn:"10s"})
+        res.json(token)
     }else{
-        res.json({error:"Datos invalidos"})
+        res.json({error:"Datos invalidos o cuenta no verificada"})
     }
+})
+
+app.post("/register", (req,res) => {
+    const {user,pass} = req.body
+    if(user.length < 1 || pass.length < 1){ return res.status(400).json({error:"Los campos no pueden estar vacios"})}
+    const userFind = User.find(u => u.user == user)
+    if(userFind){ return res.status(400).json({error:"Ese usuario ya existe"})}
+    const toke = jwt.sign(req.body, "secret")
+    const token = toke.replaceAll(".","@")
+    obj={
+        user,
+        pass,
+        verified:false,
+        banned:false,
+        token
+    }
+    User.push(obj)
+    res.status(200).json(obj.token)
 })
 
 app.get("/", (req,res) => {
---
+++ db.js
@@ -0,0 +1,21 @@
+module.exports = { 
+    User:[
+    {
+        user:"edgar",
+        pass:"123",
+        verified:true,
+        banned:false,
+    },
+    {
+        user:"yina",
+        pass:"123",
+        verified:false,
+        banned:false,
+    },
+    {
+        user:"pedro",
+        pass:"123",
+        verified:false,
+        banned:false,
+    }
+]}
