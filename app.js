b9cb7f380993ae992adc562b0a1a967ff0ab7fd2
edgarrios412 <edgarrios412@gmail.com>
2023-04-17 17:47:55 -0500

    Push

--- app.js
+++ app.js
@@ -81,5 +81,5 @@ app.post("/auth", (req,res) => {
 })
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server listening on port 3000")
+    console.log("Server listening on port 3000");
 })
